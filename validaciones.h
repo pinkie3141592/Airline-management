17de75915dc0255b08df19f2e2db887c2a6b8e83
Rebeca <rebecasanchez2513@gmail.com>
2026-04-09 00:28:53 -0600

    Add files via upload

---
+++ validaciones.h
@@ -0,0 +1,11 @@
+#ifndef VALIDACIONES_H
+#define VALIDACIONES_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+bool validar_cadena(char *cadena);
+bool validar_entero(int min, int max, int num);
+
+#endif
